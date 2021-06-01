387cf1f6bc91d82fd809bce83ab22d15e6240baa
jidro <z3069566588@163.com>
2021-06-01 17:53:21 +0800

    change-color

--- My-xterm/install.sh
+++ My-xterm/install.sh
@@ -3,7 +3,12 @@
 sudo mv ~/.Xdefaults ~/.Xdefaults.bak
 sudo cp .Xdefaults ~/
 
-sudo mv /etc/X11/app-defaults/XTerm/XTerm /etc/X11/app-defaults/XTerm/XTerm.bak
-sudo mv /etc/X11/app-defaults/XTerm/XTerm-color /etc/X11/app-defaults/XTerm/XTerm-color.bak
-sudo cp XTerm /etc/X11/app-defaults/XTerm/
-sudo cp XTerm-color /etc/X11/app-defaults/XTerm/
+sudo mv /etc/X11/app-defaults/XTerm /etc/X11/app-defaults/XTerm.bak
+sudo mv /etc/X11/app-defaults/XTerm-color /etc/X11/app-defaults/XTerm-color.bak
+sudo mv /etc/X11/app-defaults/UXTerm /etc/X11/app-defaults/UXTerm.bak
+sudo mv /etc/X11/app-defaults/UXTerm-color /etc/X11/app-defaults/UXTerm-color.bak
+
+sudo cp XTerm /etc/X11/app-defaults/
+sudo cp XTerm-color /etc/X11/app-defaults/
+sudo cp UXTerm /etc/X11/app-defaults/
+sudo cp UXTerm-color /etc/X11/app-defaults/
