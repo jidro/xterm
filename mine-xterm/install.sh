8f78b238cdcbb00bc34f3db957c4b01c9b27db65
Jidro.Z <zoujizhuo524@163.com>
2023-04-01 15:51:18 +0800

    Synchroninzing app-default with Xresources.

--- mine-xterm/install.sh
+++ mine-xterm/install.sh
@@ -5,16 +5,16 @@ mv -v ~/.Xresources ~/.Xresources.bak
 cp -rfpv .Xresources ~/
 
 # Back up the xterm and uxterm system configuration.
-#sudo mv -v /etc/X11/app-defaults/XTerm /etc/X11/app-defaults/XTerm.bak
-#sudo mv -v /etc/X11/app-defaults/XTerm-color /etc/X11/app-defaults/XTerm-color.bak
-#sudo mv -v /etc/X11/app-defaults/UXTerm /etc/X11/app-defaults/UXTerm.bak
-#sudo mv -v /etc/X11/app-defaults/UXTerm-color /etc/X11/app-defaults/UXTerm-color.bak
+sudo mv -v /etc/X11/app-defaults/XTerm /etc/X11/app-defaults/XTerm.bak
+sudo mv -v /etc/X11/app-defaults/XTerm-color /etc/X11/app-defaults/XTerm-color.bak
+sudo mv -v /etc/X11/app-defaults/UXTerm /etc/X11/app-defaults/UXTerm.bak
+sudo mv -v /etc/X11/app-defaults/UXTerm-color /etc/X11/app-defaults/UXTerm-color.bak
 
 # Change the xterm and uxterm system configuration to the repository configuration file.
-#sudo cp -rfpv XTerm /etc/X11/app-defaults/
-#sudo cp -rfpv XTerm-color /etc/X11/app-defaults/
-#sudo cp -rfpv UXTerm /etc/X11/app-defaults/
-#sudo cp -rfpv UXTerm-color /etc/X11/app-defaults/
+sudo cp -rfpv XTerm /etc/X11/app-defaults/
+sudo cp -rfpv XTerm-color /etc/X11/app-defaults/
+sudo cp -rfpv UXTerm /etc/X11/app-defaults/
+sudo cp -rfpv UXTerm-color /etc/X11/app-defaults/
 
 # Make the configuration effective
 #[[ -f ~/.Xresources ]] && xrdb -merge -I$HOME ~/.Xresources
