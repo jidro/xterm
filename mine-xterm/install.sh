e99ecaa4a386d63ff5e79775b5deae9279d2ec02
Jidro.Z <zoujizhuo524@163.com>
2023-04-02 09:31:27 +0800

    Error in modifying the install file

--- mine-xterm/install.sh
+++ mine-xterm/install.sh
@@ -1,5 +1,9 @@
 #!/bin/env bash
 
+# It is possible that there is no /etc/X11/appdefaults/ folder. 
+# If there is no such folder, create it
+sudo mkdir -pv /etc/X11/appdefaults/
+
 # Change the xterm individual user configuration to the repository's configuration file.
 mv -v ~/.Xresources ~/.Xresources.bak
 cp -rfpv .Xresources ~/
