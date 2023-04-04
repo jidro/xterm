bf997371ef7948ac6d0a88e6fbcd5918e0f30bd7
JidroZ <z3069566588@163.com>
2023-04-04 14:28:32 +0000

    Add font family and Delete unnecessary files

--- antiX-xterm/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-mv -v ~/.Xresources ~/.Xresources.bak
-cp -rfpv .Xresources ~/
-
-mv -v ~/.config/xresources.d/xcalc/xcalc.conf ~/.config/xresources.d/xcalc/xcalc.conf.bak
-mv -v ~/.config/xresources.d/xcolours/antix.conf ~/.config/xresources.d/xcolours/antix.conf.bak
-mv -v ~/.config/xresources.d/xcolours/zenburn.conf ~/.config/xresources.d/xcolours/zenburn.conf.bak
-cp -rfpv xresources.d ~/.config/
